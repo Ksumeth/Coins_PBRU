565e28f507cb748881e7ca8e6f22af7501fb7806
unknown <kosanun@gmail.com>
2015-08-20 13:52:24 +0700

    Start app

---
+++ android/src/apppbru/sumeth/pbru/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package apppbru.sumeth.pbru.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import apppbru.sumeth.pbru.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxGame(), config);
+	}
+}
